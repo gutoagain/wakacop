f07e5715c4b2d2638884af3baf43a4661653ca4a
Marcio <marcio.augusto.o.s@gmail.com>
2024-05-11 11:48:39 -0300

    implements flow repository pauta

--- src/main/java/academy/wakanda/wakacop/pauta/domain/Pauta.java
+++ src/main/java/academy/wakanda/wakacop/pauta/domain/Pauta.java
@@ -1,7 +1,10 @@
 package academy.wakanda.wakacop.pauta.domain;
 
+import academy.wakanda.wakacop.pauta.application.api.NovaPautaRequest;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
@@ -10,6 +13,7 @@ import java.util.UUID;
 @Getter
 @ToString
 @Entity
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Pauta {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,4 +24,10 @@ public class Pauta {
     private UUID idAssociadoAutor;
     private LocalDateTime dataCriacao;
 
+    public Pauta(NovaPautaRequest novaPauta) {
+        this.titulo = novaPauta.getTitulo();
+        this.descricao = novaPauta.getDescricao();
+        this.idAssociadoAutor = novaPauta.getIdAssociadoAutor();
+        this.dataCriacao = LocalDateTime.now();
+    }
 }
